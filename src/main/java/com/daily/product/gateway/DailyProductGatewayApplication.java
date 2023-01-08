7b2dab60cb62552879133c7bfe0bb3fb0fe03d72
VenhMac <gpfm312@gmail.com>
2023-01-09 00:18:17 +0900

    initialize project

---
+++ src/main/java/com/daily/product/gateway/DailyProductGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.daily.product.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyProductGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyProductGatewayApplication.class, args);
+	}
+
+}
